fb044402f34709715d13738700bdbc441bec93a5
Tran Van Kieu <kieutv107@gmail.com>
2019-01-14 14:51:22 +0700

    ##

--- app/constants.py
+++ app/constants.py
@@ -9,6 +9,7 @@ STATUS_BAD_REQUEST = 400
 STATUS_UNAUTHORIZED = 401
 STATUS_FORBIDDEN = 403
 
+
 # Messages
 MESG_STUDY_NOT_FOUND = "Study name already exists."
 MESG_STUDY_SHARE_YOURSELF = "Should not need share yourself."
